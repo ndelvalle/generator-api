2db5df96cf22d31707bb98e9c95d1cb885ae2d3c
ndelvalle <nicolas.delvalle@gmail.com>
2016-08-14 17:37:18 +0800

    Use express status end instead of send when data is not send

--- generators/app/templates/libraries/controller.js
+++ generators/app/templates/libraries/controller.js
@@ -34,7 +34,7 @@ class Controller {
   update(req, res, next) {
     this.model.update(req.params.id, req.body)
     .then(doc => {
-      if (!doc) { return res.status(404).send(); }
+      if (!doc) { return res.status(404).end(); }
       return res.status(200).json(doc);
     })
     .catch(err => next(err));
