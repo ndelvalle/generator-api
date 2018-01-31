d6c5756c56556832543fb7dd319e3fc545f25a09
Johan <jteller@edfinc.com>
2018-01-30 20:04:16 -0500

    Router now updates with 'yo api:model'

--- generators/model/index.js
+++ generators/model/index.js
@@ -1,23 +1,24 @@
 const Generator = require('yeoman-generator');
-const to        = require('to-case');
-
+const to = require('to-case');
 
 const genModelNames = model => ({
-  slugName  : to.slug(model),
+  slugName: to.slug(model),
   pascalName: to.pascal(model),
-  camelName : to.camel(model)
+  camelName: to.camel(model)
 });
 
 const serverGenerator = Generator.extend({
   prompting: {
     ask() {
-      return this.prompt([{
-        name   : 'models',
-        type   : 'input',
-        message: 'Models: (singular and comma separated)',
-        filter : answer => answer.split(','),
-        default: 'newModel'
-      }]).then((answers) => {
+      return this.prompt([
+        {
+          name: 'models',
+          type: 'input',
+          message: 'Models: (singular and comma separated)',
+          filter: answer => answer.split(','),
+          default: 'newModel'
+        }
+      ]).then((answers) => {
         this.models = answers.models.map(genModelNames);
       });
     }
@@ -26,49 +27,67 @@ const serverGenerator = Generator.extend({
   writing: {
     model() {
       this.models.forEach((model) => {
-
         const controllerPath = `model/${model.slugName}/controller.js`;
-        const facadePath     = `model/${model.slugName}/facade.js`;
-        const routerPath     = `model/${model.slugName}/router.js`;
-        const schemaPath     = `model/${model.slugName}/schema.js`;
+        const facadePath = `model/${model.slugName}/facade.js`;
+        const routerPath = `model/${model.slugName}/router.js`;
+        const schemaPath = `model/${model.slugName}/schema.js`;
 
         if (
           this.fs.exists(this.destinationPath(controllerPath)) ||
-          this.fs.exists(this.destinationPath(facadePath))     ||
-          this.fs.exists(this.destinationPath(routerPath))     ||
+          this.fs.exists(this.destinationPath(facadePath)) ||
+          this.fs.exists(this.destinationPath(routerPath)) ||
           this.fs.exists(this.destinationPath(schemaPath))
         ) {
           this.log(`Model ${model.slugName} already exists`);
-          return;
+        } else {
+          if (this.fs.exists(this.destinationPath('routes.js'))) {
+            let routes = this.fs.read(this.destinationPath('routes.js'));
+            routes = routes.split('\n');
+            const routerPathmini = routerPath.slice(0, -3);
+            console.log(routerPath, routerPathmini);
+            routes.splice(3, 0, `const ${model.slugName} = require('./${routerPathmini}');`);
+            const lineindex = routes.findIndex(line => line.includes('router.use'));
+            if (lineindex === -1) {
+              lineindex = 11;
+            }
+            routes.splice(lineindex, 0, `router.use('/${model.slugName}', ${model.slugName});`);
+            routes = routes.join('\n');
+
+            this.fs.write(this.destinationPath('routes.js'), routes);
+          }
+
+          this.fs.copyTpl(
+            this.templatePath('./../../app/templates/model/controller.js'),
+            this.destinationPath(`model/${model.slugName}/controller.js`),
+            {
+              model
+            }
+          );
+
+          this.fs.copyTpl(
+            this.templatePath('./../../app/templates/model/facade.js'),
+            this.destinationPath(facadePath),
+            {
+              model
+            }
+          );
+
+          this.fs.copyTpl(
+            this.templatePath('./../../app/templates/model/router.js'),
+            this.destinationPath(routerPath),
+            {
+              model
+            }
+          );
+
+          this.fs.copyTpl(
+            this.templatePath('./../../app/templates/model/schema.js'),
+            this.destinationPath(schemaPath),
+            {
+              model
+            }
+          );
         }
-
-        this.fs.copyTpl(
-          this.templatePath('./../../app/templates/model/controller.js'),
-          this.destinationPath(`model/${model.slugName}/controller.js`), {
-            model
-          }
-        );
-
-        this.fs.copyTpl(
-          this.templatePath('./../../app/templates/model/facade.js'),
-          this.destinationPath(facadePath), {
-            model
-          }
-        );
-
-        this.fs.copyTpl(
-          this.templatePath('./../../app/templates/model/router.js'),
-          this.destinationPath(routerPath), {
-            model
-          }
-        );
-
-        this.fs.copyTpl(
-          this.templatePath('./../../app/templates/model/schema.js'),
-          this.destinationPath(schemaPath), {
-            model
-          }
-        );
       });
     }
   }
